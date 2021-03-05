9ff242f5b97aab4119117d897a413ad7441ee17f
offcn-java <james@qq.com>
2021-03-05 16:32:54 +0800

    添加了一个文件哦哦哦

--- scw-common/src/main/java/com/offcn/utils/MyTest2.java
+++ scw-common/src/main/java/com/offcn/utils/MyTest2.java
@@ -2,6 +2,6 @@ package com.offcn.utils;
 
 public class MyTest2 {
     public static void main(String[] args) {
-        System.out.println("zhuzhuhu");
+        System.out.println("zhuzhuhu111");
     }
 }
